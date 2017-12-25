b9f2e6e3744fecdc82952c6deba5cd8f9c26d7bc
Tomasz Omeljaniuk <tomekomel@gmail.com>
2017-12-25 23:56:21 +0100

    initial create post form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends ('layouts.master')
+
+@section ('content')
+	<div class="col-sm-8 blog-main">
+		<h1>Publish a Post</h1>
+		<hr />
+		<form method="POST" action="/posts">
+		  <div class="form-group">
+		    <label for="title">Title:</label>
+		    <input type="text" class="form-control" id="title" name="title">
+		  </div>
+		  <div class="form-group">
+		    <label for="body">Body</label>
+		    <textarea id="body" name="body" class="form-control"></textarea>
+		  </div>
+		  <button type="submit" class="btn btn-primary">Publish</button>
+		</form>
+	</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
