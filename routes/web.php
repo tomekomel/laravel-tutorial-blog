55e2ba31fe83ed60bc42db5bf70d70a8ea4403ca
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-07 00:41:08 +0200

    Add new route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ Route::get('/about', function () {
 Route::get('/posts', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
+Route::get('/posts/{post}', 'PostsController@show');
