418b716bab5b4b37316abb3d6a5a66813f7fd317
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-08 00:43:36 +0200

    Add validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -24,17 +24,12 @@ class PostsController extends Controller
 
     public function store()
     {
-        /*$post = new Post;
-
-        $post->title = request('title');
-        $post->body = request('body');
-
-        $post->save();*/
-
-        Post::create([
-            'title' => request('title'),
-            'body' => request('body'),
+        $this->validate(request(), [
+            'title' => 'required',
+            'body' => 'required',
         ]);
+
+        Post::create(request(['title', 'body']));
 
         return redirect('/posts');
     }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,5 +16,15 @@
 			</div>
 			<button type="submit" class="btn btn-primary">Publish</button>
 		</form>
+
+		@if (count($errors))
+			<div class="form-group">
+				<div class="alert alert-danger">
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</div>
+			</div>
+		@endif
 	</div>
 @endsection
