e8724eed5de707ffeb1b73fd668bb5785d822b89
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-13 22:33:16 +0200

    Add registering new user methods, small fix in db

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,5 +3,24 @@
 @section ('content')
     <div class="col-sm-8 blog-main">
         <h1>Register</h1>
+
+        <form method="POST" action="/register">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" name="name" id="name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" name="email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Register</button>
+            </div>
+        </form>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,14 @@ Route::get('/about', function () {
 	return view('about');
 });
 
-Route::get('/posts', 'PostsController@index');
+Route::get('/posts', 'PostsController@index')->name('home');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
 
 Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
 Route::get('/login', 'SessionsController@create');
+Route::get('/logout', 'SessionsController@destroy');
