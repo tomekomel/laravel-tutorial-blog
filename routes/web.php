1a5acd7829eb48c20308cd1bfefaacc1cfc35bda
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-13 22:00:37 +0200

    Add section sceleton

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::get('/login', 'SessionsController@create');
