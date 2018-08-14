12563a2b28d0d927987a5493bf11fb9652b33caf
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-15 00:29:21 +0200

    Add password confirmation

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -17,7 +17,7 @@ class RegistrationController extends Controller
         $this->validate(request(), [
             'name' => 'required',
             'email' => 'required|email',
-            'password' => 'required',
+            'password' => 'required|confirmed',
         ]);
 
         $user = User::create(request(['name', 'email', 'password']));
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -8,19 +8,25 @@
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="name">Name:</label>
-                <input type="text" class="form-control" name="name" id="name">
+                <input type="text" class="form-control" name="name" id="name" required>
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" class="form-control" name="email" id="email">
+                <input type="email" class="form-control" name="email" id="email" required>
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" class="form-control" name="password" id="password">
+                <input type="password" class="form-control" name="password" id="password" required>
+            </div>
+            <div class="form-group">
+                <label for="password_confirmation">Password Confirmation:</label>
+                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" required>
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-primary">Register</button>
             </div>
+
+            @include('layouts.errors')
         </form>
     </div>
 @endsection
