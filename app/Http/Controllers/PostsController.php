3574e9106781ad3948a77377a92121642989f99e
Tomasz Omeljaniuk <tomekomel@gmail.com>
2018-08-22 00:59:34 +0200

    View Composer for Archives

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -22,12 +22,6 @@ class PostsController extends Controller
         }
         $posts = $posts->get();
 
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
-
         return view('posts.index', compact('posts', 'archives'));
     }
 
